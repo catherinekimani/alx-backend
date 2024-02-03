754cd9c2df8a5879a2c69f1c5bf37cb60c6b7b8f
catherinekimani <catherinekimani882@gmail.com>
2024-02-03 22:10:04 +0300

    Feat:  Create a class named BasicCache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""  Basic dictionary """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+    def put(self, key, item):
+        """ Add item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key"""
+        if key is not None:
+            return self.cache_data.get(key)
