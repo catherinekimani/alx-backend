f01a36a9e068de76de18b05c9978abfae558fce9
catherinekimani <catherinekimani882@gmail.com>
2024-02-11 22:46:14 +0300

    modify code

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
 app.url_map.strict_slashes(False)
@@ -13,6 +14,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 
