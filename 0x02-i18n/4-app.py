a34ac132beb7eb2b06f8606857809dcb20e04630
catherinekimani <catherinekimani882@gmail.com>
2024-02-11 23:00:16 +0300

    Implement locale param handling

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Parametrize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class """
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+app.url_map.strict_slashes(False)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ locale selector """
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """ route """
+    return render_template("4-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
