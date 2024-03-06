7744d7de152e52de59ba451f91e448262e0fb5e8
catherinekimani <catherinekimani882@gmail.com>
2024-03-06 20:38:33 +0300

    Create createPushNotificationsJobs func

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,23 @@
+function createPushNotificationsJobs (jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw Error('Jobs is not an array');
+  }
+
+  jobs.forEach((theJob) => {
+    const job = queue.create('push_notification_code_3', theJob);
+
+    job.on('complete', function () {
+      console.log(`Notification job ${job.id} completed`);
+    }).on('failed', function (error) {
+      console.log(`Notification job ${job.id} failed: ${error}`);
+    }).on('progress', function (progress, data) {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+
+    job.save((error) => {
+      if (!error) console.log(`Notification job created: ${job.id}`);
+    });
+  });
+}
+
+module.exports = createPushNotificationsJobs;
